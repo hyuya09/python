3bdeec9c83d881db77c743556e35b13f716012cf
hyuya09 <hyuyafukui1998@gmail.com>
2023-08-07 22:47:57 +0900

    データの取得＆csvファイル出力

--- scraping/scraping_practice.py
+++ scraping/scraping_practice.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
+import pandas as pd
 
 options = Options()
 #CUIで実行(画面が表示されずに実行される)
@@ -23,3 +24,30 @@ sleep(1)
 
 #ログインボタンを自動でクリック
 elem_login_btn = browser.find_element(By.ID,'login-btn').click()
+
+#テキストの取得
+name = browser.find_element(By.ID, 'name').text
+company = browser.find_element(By.ID, 'company').text
+birthday = browser.find_element(By.ID, 'birthday').text
+hobby = browser.find_element(By.ID, 'hobby').text
+
+#テキストの複数取得
+elems_th = browser.find_elements(By.TAG_NAME, 'th')
+elems_td = browser.find_elements(By.TAG_NAME, 'td')
+
+keys = []
+for elem_th in elems_th:
+    key = elem_th.text
+    keys.append(key)
+
+values = []
+for elem_td in elems_td:
+    value = elem_td.text
+    values.append(value)
+
+
+#CSVファイルへ出力
+df = pd.DataFrame()
+df['項目'] = keys
+df['値'] = values
+df.to_csv('講師情報.csv', index = False) #index = False->インデックスが必要ない場合
